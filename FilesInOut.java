6477d64fb209218e14ec877ea7c21f98142dfb1f
ska4112001 <64066740+ska4112001@users.noreply.github.com>
2023-04-05 09:09:21 +0200

    added scanner to read and print the file and output it to the console

--- FilesInOut.java
+++ FilesInOut.java
@@ -1,3 +1,4 @@
+package checkpoint4;
 import java.io.*;
 import java.awt.*;
 import java.awt.event.*;
@@ -12,16 +13,22 @@ import java.lang.Number;
  */
 public class FilesInOut {
 
-    public static void main(String[] args) {
-        // Replace this with statements to set the file name (input) and file name (output).
+    public static void main(String[] args) throws FileNotFoundException {
+        File inputFile = new File("C:\\Users\\samia\\eclipse-workspace\\Checkpoint4\\src\\checkpoint4\\input.txt");
         // Initially it will be easier to hardcode suitable file names.
 
         // Set up a new Scanner to read the input file.
+        Scanner in = new Scanner(inputFile);
         // Processing line by line would be sensible here.
-        // Initially, echo the text to System.out to check you are reading correctly.
+     // Initially, echo the text to System.out to check you are reading correctly.
+        while(in.hasNextLine()){
+        	String line = in.nextLine();
+        	System.out.println(line);
+        }
+        
         // Then add code to modify the text to the output format.
-
         // Set up a new PrintWriter to write the output file.
+        PrintWriter out = new PrintWriter("output.txt");
         // Add suitable code into the above processing (because you need to do this line by line also.
         // That is, read a line, write a line, loop.
 
